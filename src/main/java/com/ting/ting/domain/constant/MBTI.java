8ab15f137c9c42dea34164f4dd14a3caf5cf90e9
sol <sadlthf@icloud.com>
2023-04-24 17:48:05 +0900

    #1 - MBTI enum 추가

---
+++ src/main/java/com/ting/ting/domain/constant/MBTI.java
@@ -0,0 +1,21 @@
+package com.ting.ting.domain.constant;
+
+public enum MBTI {
+
+    ENFJ,
+    ENFP,
+    ENTJ,
+    ENTP,
+    ESFJ,
+    ESFP,
+    ESTJ,
+    ESTP,
+    INFJ,
+    INFP,
+    INTJ,
+    INTP,
+    ISFJ,
+    ISFP,
+    ISTJ,
+    ISTP
+}
