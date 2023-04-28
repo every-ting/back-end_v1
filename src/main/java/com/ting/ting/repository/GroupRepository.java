548cfe7f256cbb4be993ad0ea89f55243e123407
sol <sadlthf@icloud.com>
2023-04-28 09:01:08 +0900

    #1 - 과팅 팀 생성 비즈니즈 로직 구현
    custom exception 구현 후 추후 exception 처리를 추가할 예정입니다.

---
+++ src/main/java/com/ting/ting/repository/GroupRepository.java
@@ -0,0 +1,7 @@
+package com.ting.ting.repository;
+
+import com.ting.ting.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
