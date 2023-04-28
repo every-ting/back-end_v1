34dcb778908a68bb6c096d63bcf2b0642b0d1ac1
sol <sadlthf@icloud.com>
2023-04-28 20:07:56 +0900

    #8 - 과팅 조회, 참가, 참가 요청 삭제 컨트롤러 구현
    * custom exception 과 custom response 를 구현한 후 좀 더 다듬을 예정입니다. 지금은 일단 반환 값이 없는 경우 문자열로 "success"를 리턴하도록 했습니다.

--- src/main/java/com/ting/ting/controller/GroupController.java
+++ src/main/java/com/ting/ting/controller/GroupController.java
@@ -1,12 +1,13 @@
 package com.ting.ting.controller;
 
 import com.ting.ting.dto.request.GroupRequest;
+import com.ting.ting.dto.response.GroupResponse;
 import com.ting.ting.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -15,9 +16,26 @@ public class GroupController {
 
     private final GroupService groupService;
 
+    @GetMapping
+    public Page<GroupResponse> list(Pageable pageable) {
+        return groupService.list(pageable).map(GroupResponse::from);
+    }
+
     @PostMapping
     public String create(@RequestBody GroupRequest request) {
         groupService.saveGroup(request.toDto());
+        return "success";
+    }
+
+    @PostMapping("/request/{groupId}")
+    public String request(@PathVariable long groupId) {
+        groupService.createJoinRequest(groupId);
+        return "success";
+    }
+
+    @DeleteMapping("/request/{groupId}")
+    public String deleteRequest(@PathVariable long groupId) {
+        groupService.deleteJoinRequest(groupId);
         return "success";
     }
 }
