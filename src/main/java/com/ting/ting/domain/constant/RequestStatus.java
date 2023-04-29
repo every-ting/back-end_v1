2382060a7d290390b3e5f3b6c288fc6f26361911
good0619@naver.com <32181771@dankook.ac.kr>
2023-04-30 00:52:22 +0900

    #4 - Blind Request Entity 생성

---
+++ src/main/java/com/ting/ting/domain/constant/RequestStatus.java
@@ -0,0 +1,7 @@
+package com.ting.ting.domain.constant;
+
+public enum RequestStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
---
+++ src/main/java/com/ting/ting/repository/BlindRequestRepository.java
@@ -0,0 +1,8 @@
+package com.ting.ting.repository;
+
+import com.ting.ting.domain.BlindRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlindRequestRepository extends JpaRepository<BlindRequest, Long> {
+
+}
