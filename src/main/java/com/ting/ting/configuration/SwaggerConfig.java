5314daa1fe3873b3e0d7e604a5d07a044cb6d18a
sol <sadlthf@icloud.com>
2023-05-02 18:48:06 +0900

    #12 - config 파일

---
+++ src/main/java/com/ting/ting/configuration/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.ting.ting.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Ting app API",
+                version = "0.0"
+        )
+)
+public class SwaggerConfig {
+}
