90681481ef0ed24569417db41cea9906de7a4dea
good0619@naver.com <32181771@dankook.ac.kr>
2023-05-18 17:03:53 +0900

    #80 - 소개팅 성공 횟수 조회 로직 추가

--- src/main/java/com/ting/ting/repository/BlindDateRepository.java
+++ src/main/java/com/ting/ting/repository/BlindDateRepository.java
@@ -1,7 +1,13 @@
 package com.ting.ting.repository;
 
 import com.ting.ting.domain.BlindDate;
+import com.ting.ting.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BlindDateRepository extends JpaRepository<BlindDate, Long> {
+
+    @Query(value = "SELECT count(bd) FROM BlindDate bd WHERE bd.menUser = :fromUser OR bd.womenUser = :fromUser")
+    Long countByBlindDate(@Param("fromUser") User user);
 }
