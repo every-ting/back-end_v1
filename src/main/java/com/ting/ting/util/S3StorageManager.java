6c7c9abd468dec8066a51853e99a67505438a7cf
sol <sadlthf@icloud.com>
2023-05-21 15:35:01 +0900

    #94 - s3 이미지 삭제 메소드 구현

--- src/main/java/com/ting/ting/util/S3StorageManager.java
+++ src/main/java/com/ting/ting/util/S3StorageManager.java
@@ -1,6 +1,7 @@
 package com.ting.ting.util;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,7 +19,6 @@ public class S3StorageManager {
     private final AmazonS3 amazonS3;
 
     public String uploadByteArrayToS3WithKey(byte[] imageBytes, String key) {
-        // Create metadata for the object
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType("image/png");
         metadata.setContentLength(imageBytes.length);
@@ -28,6 +28,10 @@ public class S3StorageManager {
         amazonS3.putObject(bucketName, key, inputStream, metadata);
 
         return getImageUrlForKey(key);
+    }
+
+    public void deleteImageByKey(String key) throws AmazonS3Exception {
+        amazonS3.deleteObject(bucketName, key);
     }
 
     public String getImageUrlForKey(String key) {
