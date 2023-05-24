c284fe0f9a1633d03bc8a6c8f253d1366f8a0e3d
sol <sadlthf@icloud.com>
2023-05-24 13:45:40 +0900

    #76 - 성별이 다른 팀 조회 컨트롤러 구현

--- src/main/java/com/ting/ting/controller/GroupController.java
+++ src/main/java/com/ting/ting/controller/GroupController.java
@@ -16,7 +16,13 @@ public interface GroupController {
      * 같은 성별 이면서 내가 속한 팀이 아닌 팀 조회
      */
     @GetMapping
-    Response<Page<GroupWithRequestStatusResponse>> getSuggestedSameGenderGroupList(@ParameterObject Pageable pageable);
+    Response<Page<GroupWithRequestStatusResponse>> getJoinableSameGenderGroupList(@ParameterObject Pageable pageable);
+
+    /**
+     * 다른 성별의 팀 조회
+     */
+    @GetMapping("/{groupId}/opposite-gender-groups")
+    Response<Page<GroupWithLikeStatusResponse>> getOppositeGenderGroupList(@PathVariable Long groupId, @ParameterObject Pageable pageable);
 
     /**
      * 내가 속한 팀 조회
@@ -57,26 +63,26 @@ public interface GroupController {
     /**
      * 팀장 - 했던 초대 모두 조회
      */
-    @GetMapping("{groupId}/members/invitations")
+    @GetMapping("/{groupId}/members/invitations")
     Response<Set<GroupInvitationResponse>> getGroupMemberInvitationList(@PathVariable Long groupId);
 
     /**
      * 팀장 - 과팅 팀 초대
      */
-    @PostMapping("{groupId}/members/invitations")
+    @PostMapping("/{groupId}/members/invitations")
     Response<GroupInvitationResponse> createGroupMemberInvitation(@PathVariable Long groupId);
 
     /**
      * 팀장 - 과팅 팀 초대 취소
      */
-    @DeleteMapping("{groupId}/members/invitations/{groupInvitationId}")
+    @DeleteMapping("/{groupId}/members/invitations/{groupInvitationId}")
     Response<Void> cancelGroupMemberInvitation(@PathVariable Long groupId, @PathVariable Long groupInvitationId);
 
     /**
      * 과팅 팀 초대 수락
      */
     // TODO : 일단 GetMapping으로 -> 프론트 url 정해지면 PutMapping으로 바꿀 예정.
-    @GetMapping("{groupId}/members/invitations/{invitationCode}")
+    @GetMapping("/{groupId}/members/invitations/{invitationCode}")
     Response<Void> acceptGroupMemberInvitation(@PathVariable Long groupId, @PathVariable String invitationCode);
 
     /**
@@ -94,13 +100,13 @@ public interface GroupController {
     /**
      * 팀장 - 팀 멤버 가입 요청 수락
      */
-    @PostMapping("members/requests/{groupMemberRequestId}")
+    @PostMapping("/members/requests/{groupMemberRequestId}")
     Response<GroupMemberResponse> acceptJoinRequestToMyGroup(@PathVariable Long groupMemberRequestId);
 
     /**
      * 팀장 - 팀 멤버 가입 요청 거절
      */
-    @DeleteMapping("members/requests/{groupMemberRequestId}")
+    @DeleteMapping("/members/requests/{groupMemberRequestId}")
     Response<Void> rejectJoinRequestToMyGroup(@PathVariable Long groupMemberRequestId);
 
     /**
