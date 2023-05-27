baa046fa5b216c2005725f02c8a3f81e87efcfb7
sol <sadlthf@icloud.com>
2023-05-27 19:15:23 +0900

    #104 - 같은 성별의 팀 찜 저장하는 테이블 정의 - `group_like_to_join`

---
+++ src/main/java/com/ting/ting/domain/GroupLikeToJoin.java
@@ -0,0 +1,36 @@
+package com.ting.ting.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Table(name = "\"group_like_to_join\"", uniqueConstraints = {
+        @UniqueConstraint(name = "unique_group_and_user", columnNames = {"from_user_id", "to_group_id"}),
+})
+@Entity
+public class GroupLikeToJoin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "from_user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User fromUser;
+
+    @JoinColumn(name = "to_group_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Group toGroup;
+
+    protected GroupLikeToJoin() {}
+
+    private GroupLikeToJoin(User fromUser, Group toGroup) {
+        this.fromUser = fromUser;
+        this.toGroup = toGroup;
+    }
+
+    public static GroupLikeToJoin of(User fromUser, Group toGroup) {
+        return new GroupLikeToJoin(fromUser, toGroup);
+    }
+}
