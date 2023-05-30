9719c8f244a4171cf6703eca32d693e6c0f042d4
good0619@naver.com <32181771@dankook.ac.kr>
2023-05-31 03:34:59 +0900

    #129 - 유저 정보를 추출하기 위한 dto 설정

--- src/main/java/com/ting/ting/dto/kakao/KakaoTokenResponse.java
+++ src/main/java/com/ting/ting/dto/kakao/KakaoTokenResponse.java
@@ -1,9 +1,10 @@
-package com.ting.ting.dto;
+package com.ting.ting.dto.kakao;
 
 import lombok.Data;
 
 @Data
 public class KakaoTokenResponse {
+
     private String access_token;
     private String token_type;
     private String refresh_token;
