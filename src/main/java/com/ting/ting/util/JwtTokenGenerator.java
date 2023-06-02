4c37a4ea0934a249b9dacb7bac461ea4c72d6d04
good0619@naver.com <32181771@dankook.ac.kr>
2023-06-03 04:13:00 +0900

    #139 - 토큰 검증 로직 추가

--- src/main/java/com/ting/ting/util/JwtTokenGenerator.java
+++ src/main/java/com/ting/ting/util/JwtTokenGenerator.java
@@ -1,7 +1,9 @@
 package com.ting.ting.util;
 
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
+import com.ting.ting.exception.ErrorCode;
+import com.ting.ting.exception.ServiceType;
+import com.ting.ting.exception.TingApplicationException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
 import java.nio.charset.StandardCharsets;
@@ -31,6 +33,21 @@ public class JwtTokenGenerator {
                 .setSubject("user-auto")
                 .signWith(key)
                 .compact();
+    }
+
+    public boolean validToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(this.key).build().parseClaimsJws(token).getBody();
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            throw new TingApplicationException(ErrorCode.TOKEN_ERROR, ServiceType.UTIL, "Invalid JWT signature.");
+        } catch (ExpiredJwtException e) {
+            throw new TingApplicationException(ErrorCode.TOKEN_ERROR, ServiceType.UTIL, "Expired JWT token.");
+        } catch (UnsupportedJwtException e) {
+            throw new TingApplicationException(ErrorCode.TOKEN_ERROR, ServiceType.UTIL, "Unsupported JWT token.");
+        } catch (IllegalArgumentException e) {
+            throw new TingApplicationException(ErrorCode.TOKEN_ERROR, ServiceType.UTIL, "Invalid JWT token");
+        }
     }
 }
 
