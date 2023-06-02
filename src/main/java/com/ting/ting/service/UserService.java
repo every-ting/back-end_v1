f0f87f1a9d7a3b1f6bf22b2a6cd80533be8d55fc
good0619@naver.com <32181771@dankook.ac.kr>
2023-06-03 04:15:34 +0900

    #139 - 로그인 성공시 토큰 생성 로직 추가 구현

--- src/main/java/com/ting/ting/service/UserService.java
+++ src/main/java/com/ting/ting/service/UserService.java
@@ -1,31 +1,48 @@
 package com.ting.ting.service;
 
+import com.ting.ting.domain.User;
 import com.ting.ting.dto.response.LogInResponse;
+import com.ting.ting.exception.ErrorCode;
+import com.ting.ting.exception.ServiceType;
 import com.ting.ting.repository.UserRepository;
+import com.ting.ting.util.JwtTokenGenerator;
 import com.ting.ting.util.KakaoInfoGenerator;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserService {
+public class UserService extends AbstractService{
 
     private final UserRepository userRepository;
     private final KakaoInfoGenerator kakaoInfoGenerator;
+    private final JwtTokenGenerator jwtTokenGenerator;
 
-    public UserService(UserRepository userRepository, KakaoInfoGenerator kakaoInfoGenerator) {
+    public UserService(UserRepository userRepository, KakaoInfoGenerator kakaoInfoGenerator, JwtTokenGenerator jwtTokenGenerator) {
+        super(ServiceType.USER);
         this.userRepository = userRepository;
         this.kakaoInfoGenerator = kakaoInfoGenerator;
+        this.jwtTokenGenerator = jwtTokenGenerator;
     }
 
     public LogInResponse logIn(String code) {
         String socialEmail = getSocialEmailByCode(code);
 
         return userRepository.findBySocialEmail(socialEmail)
-                .map(response -> new LogInResponse(true))
+                .map(response -> new LogInResponse(true, createToken(socialEmail)))
                 .orElse(new LogInResponse(false));
     }
 
-    public String getSocialEmailByCode(String code) {
+    private String createToken(String socialEmail) {
+        User user = getUserBySocialEmail(socialEmail);
+        return jwtTokenGenerator.createTokenById(user.getId());
+    }
+
+    private String getSocialEmailByCode(String code) {
         String accessToken = kakaoInfoGenerator.getKakaoTokenResponse(code).getAccess_token();
         return kakaoInfoGenerator.getKakaoUserInfoResponse(accessToken).getKakao_account().getEmail();
+    }
+
+    private User getUserBySocialEmail(String socialEmail) {
+        return userRepository.findBySocialEmail(socialEmail).orElseThrow(() ->
+                throwException(ErrorCode.USER_NOT_FOUND, String.format("[%s]의 유저 정보가 존재하지 않습니다.", socialEmail)));
     }
 }
