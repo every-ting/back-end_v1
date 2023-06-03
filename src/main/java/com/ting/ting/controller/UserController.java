8129cf9989c41009da11b9390e475aea2a8118da
good0619@naver.com <32181771@dankook.ac.kr>
2023-06-04 08:49:51 +0900

    #135 - user controller 인터페이스 & 구현부 분리

--- src/main/java/com/ting/ting/controller/UserController.java
+++ src/main/java/com/ting/ting/controller/UserController.java
@@ -4,28 +4,20 @@ import com.ting.ting.dto.request.SignUpRequest;
 import com.ting.ting.dto.response.LogInResponse;
 import com.ting.ting.dto.response.Response;
 import com.ting.ting.dto.response.SignUpResponse;
-import com.ting.ting.exception.ServiceType;
-import com.ting.ting.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/ting")
-@RestController
-public class UserController extends AbstractController {
+public interface UserController {
 
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        super(ServiceType.USER);
-        this.userService = userService;
-    }
-
+    /**
+     * 로그인 구현
+     */
     @GetMapping("/logIn")
-    public Response<LogInResponse> logIn(@RequestParam String code) {
-        return success(userService.logIn(code));
-    }
+    Response<LogInResponse> logIn(@RequestParam String code);
 
+    /**
+     * 회원가입 구현
+     */
     @PostMapping("/signUp")
-    public Response<SignUpResponse> signUp(@RequestBody SignUpRequest request) {
-        return success(userService.signUp(request));
-    }
+    Response<SignUpResponse> signUp(@RequestBody SignUpRequest request);
 }
