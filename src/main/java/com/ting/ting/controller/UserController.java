8a4bb0900a6dbf6f53e1307b4e9532cd8e371cc8
good0619@naver.com <32181771@dankook.ac.kr>
2023-06-03 21:23:35 +0900

    #126 - 회원가입 controller 로직 구현

--- src/main/java/com/ting/ting/controller/UserController.java
+++ src/main/java/com/ting/ting/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.ting.ting.controller;
 
+import com.ting.ting.dto.request.SignUpRequest;
 import com.ting.ting.dto.response.LogInResponse;
 import com.ting.ting.dto.response.Response;
+import com.ting.ting.dto.response.SignUpResponse;
 import com.ting.ting.exception.ServiceType;
 import com.ting.ting.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/ting")
 @RestController
@@ -25,8 +24,8 @@ public class UserController extends AbstractController {
         return success(userService.logIn(code));
     }
 
-    @GetMapping("/test")
-    public Long test(@RequestParam String token) {
-        return userService.test(token);
+    @PostMapping("/signUp")
+    public Response<SignUpResponse> signUp(@RequestBody SignUpRequest request) {
+        return success(userService.signUp(request));
     }
 }
