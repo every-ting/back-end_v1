98ed58975df5ca8fac9013719087d6ed7ae80ffc
sol <sadlthf@icloud.com>
2023-06-06 17:54:03 +0900

    #144 - api 를 요청한 유저의 id를 securityContext에서 추출하는 메소드 정의

--- src/main/java/com/ting/ting/service/AbstractService.java
+++ src/main/java/com/ting/ting/service/AbstractService.java
@@ -4,6 +4,8 @@ import com.ting.ting.exception.ErrorCode;
 import com.ting.ting.exception.ServiceType;
 import com.ting.ting.exception.TingApplicationException;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @AllArgsConstructor
 public abstract class AbstractService {
@@ -18,5 +20,12 @@ public abstract class AbstractService {
         throw new TingApplicationException(
             errorCode, serviceType, message
         );
+    }
+
+    public Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = Long.parseLong(authentication.getName());
+
+        return userId;
     }
 }
