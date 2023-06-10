8c60c29184254c902b87d7ee2043665581c2bce4
good0619@naver.com <32181771@dankook.ac.kr>
2023-06-10 23:34:35 +0900

    #148 - 전체 서비스 로직 수정

--- src/main/java/com/ting/ting/service/BlindService.java
+++ src/main/java/com/ting/ting/service/BlindService.java
@@ -10,32 +10,32 @@ public interface BlindService {
     /**
      * 소개팅 상대편 조회(자신의 성별에 따라 조회 결과가 다름)
      */
-    Page<BlindUserWithRequestStatusAndLikeStatusResponse> blindUsersInfo(Long userId, Pageable pageable);
+    Page<BlindUserWithRequestStatusAndLikeStatusResponse> blindUsersInfo( Pageable pageable);
 
     // Todo :: 요청하기
 
     /**
      * 소개팅 상대에게 요청
      */
-    void createJoinRequest(long fromUserId, long toUserId);
+    void createJoinRequest(long toUserId);
 
     /**
      * 소개팅 상대에게 한 요청 취소
      */
-    void deleteRequestById(long userId, long toUserId);
+    void deleteRequestById(long toUserId);
 
     /**
      * 소개팅 요청 조회(받은 요청, 한 요청 모두)
      */
-    BlindRequestWithFromAndToResponse getBlindRequest(long userId);
+    BlindRequestWithFromAndToResponse getBlindRequest();
 
     /**
      * 자신에게 온 요청 수락
      */
-    void acceptRequest(long userId, long blindRequestId);
+    void acceptRequest(long blindRequestId);
 
     /**
      * 자신에게 온 요청 거절
      */
-    void rejectRequest(long userId, long blindRequestId);
+    void rejectRequest(long blindRequestId);
 }
