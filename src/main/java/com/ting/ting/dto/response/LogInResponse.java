a1bc92e943fc0d2573c90db3790fdc0985d61dbf
good0619@naver.com <32181771@dankook.ac.kr>
2023-06-14 18:01:17 +0900

    #164 - 회원가입 & 로그인 response dto 수정

--- src/main/java/com/ting/ting/dto/response/LogInResponse.java
+++ src/main/java/com/ting/ting/dto/response/LogInResponse.java
@@ -1,5 +1,6 @@
 package com.ting.ting.dto.response;
 
+import com.ting.ting.domain.constant.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ public class LogInResponse {
 
     private boolean isRegistered;
     private String socialEmail;
+    private Gender gender;
     private String token;
 
     public LogInResponse(boolean isRegistered, String socialEmail) {
